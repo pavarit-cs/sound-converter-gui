18bd6f800010e272efbabce72756c243e9600539
Pavarit Pramual <pavarit.dev@gmail.com>
2025-05-22 22:14:29 +0700

    Make DoingPart comment

--- MainGUI.py
+++ MainGUI.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 
-#Setting Main Window
+# --- Setting Main Window ---
 app = tk.Tk()
 app.title("Sound Converter")
 screen_width = app.winfo_screenwidth()
@@ -10,10 +10,20 @@ WINDOW_HEIGHT = 450
 x_coordinate = (screen_width // 2) - (WINDOW_WIDTH // 2)
 y_coordinate = (screen_height // 2) - (WINDOW_HEIGHT // 2)
 app.geometry(f'{WINDOW_WIDTH}x{WINDOW_HEIGHT}+{x_coordinate}+{y_coordinate}')
+app.resizable(False, False)
 
+# --- value for GUI ---
+input_path = tk.StringVar()
+output_path = tk.StringVar()
+output_format = tk.StringVar(value="wav")
 
+# --- Function handlers ---
+
+# --- Widgets for GUI ---
 label_greeting = tk.Label(text='Welcome to Sound Converter!')
 label_greeting.pack(pady=50)
 
-app.resizable(False, False)
+
+
+# --- Looper for tkinter ---
 app.mainloop()
--- converter.py
+++ converter.py
@@ -2,6 +2,11 @@ from pydub import AudioSegment
 import os
 
 def convert_audio_file(input_path, output_path, output_format):
+    """
+        Function for Convert file
+        return True -> success
+        return False -> fail
+    """
     if not os.path.exists(input_path):
         return False, f"INVALID : Not found file at {input_path}"
     try :
